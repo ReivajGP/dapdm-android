8f7d5ccfaca66dd6dc917f9f0e2397cc95d93028
Reivaj Gómez <reivaj.gomez@globant.com>
2022-11-08 20:46:45 -0600

    Comenzando el ejercicio 2

---
+++ PrimerProyecto/app/src/main/java/com/rgp/primerproyecto/ejercicio_dos/AnimalsManagerActivity.kt
@@ -0,0 +1,12 @@
+package com.rgp.primerproyecto.ejercicio_dos
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.rgp.primerproyecto.R
+
+class AnimalsManagerActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_animals_manager)
+    }
+}
